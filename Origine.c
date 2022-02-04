68c460442f165b941f98451a9d8279d63aec90c0
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-02-04 14:25:51 +0100

    Update Origine.c

--- Origine.c
+++ Origine.c
@@ -1,1 +1,20 @@
-
+int elabora(char *st, int d)
+{ int i,conta;
+conta=0;
+for(i=0;i<d;i++)
+if(st[i]-48<5)
+conta++;
+return conta;
+}
+main() {
+char str[32];
+int I,ris;
+do
+{
+printf("Inserisci una stringa di soli numeri\n");
+scanf("%s",str);
+ris=elabora(str,strlen(str));
+printf(" Valore= %d \n",ris);
+i++;
+} while (i<3);
+}
